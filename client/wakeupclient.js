2ec39ccf1f354b2c409ab9508125b4df9b1c8122
James EJ <james@makersacademy.com>
2013-11-28 03:21:51 +0000

    Added basic bookings model

--- client/wakeupclient.js
+++ client/wakeupclient.js
@@ -1,13 +1,28 @@
+'use strict';
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "James needs to wake up to code!";
   };
 
   Template.hello.events({
-    'click input' : function () {
+    'click input.book' : function (e) {
       // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+      e.preventDefault();
+      var el = e.target;
+      var date = el.getAttribute('data-date')
+      Meteor.call("addBooking",date,function(eror,bookingId){
+        console.log('added booking with id '+bookingId);
+      });
+      if (typeof console !== 'undefined'){
+        console.log(date);
+      }
     }
   });
 }
+
+var Bookings = new Meteor.Collection("bookings");
+
+Template.bookings.bookings = function(){
+  return Bookings.find({},{sort:{'createdAt':-1}});
+};
--- server/wakeupserver.js
+++ server/wakeupserver.js
@@ -1,5 +1,17 @@
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Bookings = new Meteor.Collection("bookings");
+
+Meteor.startup(function () {
+  // code to run on server at startup
+});
+
+Meteor.methods({
+  addBooking: function(date){
+    console.log('Adding Booking');
+    var bookingId = Bookings.insert({
+      'date': date,
+      'createdAt': new Date(),
+      'user': Meteor.userId()
+    });
+    return bookingId;
+  }
+});
