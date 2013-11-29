07f8b9f72371d99fb828d29bcbb939ba38ba9b1f
James EJ <james@makersacademy.com>
2013-11-29 13:40:03 +0000

    Cleanup console log and preventDefault

--- client/wakeupclient.js
+++ client/wakeupclient.js
@@ -1,38 +1,26 @@
 'use strict';
 
-if (Meteor.isClient) {
-  Template.main.greeting = function () {
-    return "James needs to wake up to code!";
-  };
-
   Template.main.events({
     'click .removeDate' : function (e) {
       var dateId = e.target.getAttribute('data-id');
       Meteor.call("removeDate",dateId);
     },
     'click input.book' : function (e) {
-      e.preventDefault();
-      var el = e.target;
-      var dateId = el.getAttribute('data-id')
-      Meteor.call("bookDate",dateId,function(error,bookingId){
-        console.log('added booking with id '+bookingId);
+      var dateId = e.target.getAttribute('data-id')
+      Meteor.call("bookDate",dateId,function(error,dateId){
       });
     },
     'click input.unbook' : function (e) {
-      e.preventDefault();
       var dateId = e.target.getAttribute('data-id')
-      console.log(dateId);
       Meteor.call("unbookDate",dateId,function(error,dateId){
-        console.log('unbooked booking with id '+dateId);
       });
     },
     'click #admin_date input[type="button"]': function(e){
-      e.preventDefault();
       var date =  document.getElementById('admin_date_input').value
       Meteor.call("addDate",date);
     }
   });
-}
+
 
 
 Template.bookings.dates = function(){
